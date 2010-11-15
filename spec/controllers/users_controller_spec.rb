301758a2de44a190800777af1efcbb98ea18fb26
pohlandt <pohlandt@gmail.com>
2010-11-15 15:25:05 +0100

    User Page

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -10,7 +10,7 @@ describe UsersController do
     end
 
     it "should be successful" do
-      get :show, :id => @user
+      get :show, :id => @user.id
       response.should be_success
     end
 
@@ -18,6 +18,22 @@ describe UsersController do
       get :show, :id => @user
       assigns(:user).should == @user
     end
+	
+	it "should have the right title" do
+      get :show, :id => @user
+      response.should have_selector("title", :content => @user.name)
+    end
+
+    it "should include the user's name" do
+      get :show, :id => @user
+      response.should have_selector("h1", :content => @user.name)
+    end
+
+    it "should have a profile image" do
+      get :show, :id => @user
+      response.should have_selector("h1>img", :class => "gravatar")
+    end
+	
   end
   
   describe "GET 'new'" do
